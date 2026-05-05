e6ce5d8cc9b16da33aca674f74d9e555b6193012
mgad3489-blip <mgad3489@gmail.com>
2026-05-05 12:34:58 +0200

    build: Allineamento al template Gradle del prof (lab12) - Gradle 9.4.1, QA plugin 1.173.0, shadow Gradle-9 compatible, foojay toolchain

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ plugins {
      * In order to create it, launch the "shadowJar" task.
      * The runnable jar will be found in build/libs/projectname-all.jar
      */
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.github.goooler.shadow") version "8.1.8"
 
-    id("org.danilopianini.gradle-java-qa") version "1.66.0"
+    id("org.danilopianini.gradle-java-qa") version "1.173.0"
 }
 
 repositories { // Where to search for dependencies
@@ -22,27 +22,10 @@ repositories { // Where to search for dependencies
 
 dependencies {
     // Suppressions for SpotBugs
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.6")
-
-    // Google Guava: Google Core Libraries for Java
-    implementation("com.google.guava:guava:31.1-jre")
-
-    // Maven dependencies are composed by a group name, a name and a version, separated by colons
-    implementation("com.omertron:API-OMDB:1.5")
-    implementation("org.jooq:jool:0.9.15")
-
-    /*
-     * Simple Logging Facade for Java (SLF4J) with Apache Log4j
-     * See: http://www.slf4j.org/
-     */
-    val slf4jVersion = "2.0.16"
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    // Logback backend for SLF4J
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.8")
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
 
     // JUnit API and testing engine
     val jUnitVersion = "5.11.1"
-    // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
@@ -50,6 +33,10 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClass.set("it.unibo.towerdefense.application.TowerDefense")
+}
+
+tasks.javadoc {
+    isFailOnError = false
 }
 
 tasks.test {
